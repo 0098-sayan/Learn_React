817ec2e842de4cce4a5c4d78fcb351edc05c8fbd
0098-sayan <maitysayan704@gmail.com>
2025-10-14 12:22:16 +0530

    Navbar done

---
+++ 05_UI_Project/src/comonents/section1/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full flex px-16 py-5 justify-between'>
+        <h3 className='bg-black text-white rounded-full px-6 py-2 uppercase font-medium tracking-wider'>Target Audiance</h3>
+        <button className='bg-gray-300 rounded-full px-5 py-2 text-sm uppercase font-medium'>Digital banking platfrom</button>
+    </div>
+  )
+}
+
+export default Navbar
