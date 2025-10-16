5af779e6b17428cca1230f1a1f34b144cbe0b15e
0098-sayan <maitysayan704@gmail.com>
2025-10-16 23:44:31 +0530

    Simple counter using usestate

--- 07_usestate/src/App.jsx
+++ 07_usestate/src/App.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [num, setNum] = useState(0);
 
-export default App
+  const numIncrease = () => {
+    setNum(num + 1);
+  };
+ const numDecrease = () => {
+  if (num > 0) {
+    setNum(num - 1);
+  }
+};
+
+
+  return (
+    <div>
+      <h1>The Value is {num}</h1>
+      <button onClick={numIncrease}>Increase</button>
+      <button onClick={numDecrease}>Decrease</button>
+    </div>
+  );
+};
+
+export default App;
