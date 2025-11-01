f71c347105672e164e2c46b35bafcffdc51f6232
0098-sayan <maitysayan704@gmail.com>
2025-11-01 11:46:07 +0530

    Notes app done

--- 09_notes_app/src/App.jsx
+++ 09_notes_app/src/App.jsx
@@ -3,13 +3,25 @@ import React, { useState } from "react";
 const App = () => {
   const [title, setTitle] = useState("");
   const [details, setDetails] = useState("");
+  const [task, setTask] = useState([]);
 
   const submitHandler = (e) => {
     e.preventDefault();
-    console.log(title, details);
+    const copytask = [...task];
+    copytask.push({ title, details });
+    setTask(copytask);
     setTitle("");
     setDetails("");
   };
+
+
+  const deleteNote = (idx) => {
+    const copyTask = [...task];
+    copyTask.splice(idx, 1);
+    setTask(copyTask);
+  };
+
+
   return (
     <div className="h-screen lg:flex bg-black text-white">
       <form
@@ -40,18 +52,21 @@ const App = () => {
           Add note
         </button>
       </form>
-      <div className="lg:w-1/2 lg:border-l-2  p-10">
-        <h1 className="text-4xl font-bold">Recent Notes</h1>
-        <div className="flex flex-wrap items-start justify-start gap-5 mt-6 h-[90%] overflow-auto">
-          <div className=" flex justify-between flex-col items-start relative h-52 w-40 bg-cover rounded-xl text-black pt-9 pb-4 px-4 bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')]">
-            <div>
-              <h3 className="leading-tight text-lg font-bold"></h3>
-              <p className="mt-2 leading-tight text-xs font-semibold text-gray-600"></p>
+      <div className='lg:w-1/2 lg:border-l-2  p-10'>
+        <h1 className='text-4xl font-bold'>Recent Notes</h1>
+        <div className='flex flex-wrap items-start justify-start gap-5 mt-6 h-[90%] overflow-auto'>
+          {task.map(function (elem, idx) {
+
+            return <div key={idx} className=" flex justify-between flex-col items-start relative h-52 w-40 bg-cover rounded-xl text-black pt-9 pb-4 px-4 bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')]">
+              <div>
+                <h3 className='leading-tight text-lg font-bold'>{elem.title}</h3>
+                <p className='mt-2 leading-tight text-xs font-semibold text-gray-600'>{elem.details}</p>
+              </div>
+              <button onClick={() => {
+                deleteNote(idx)
+              }} className='w-full cursor-pointer active:scale-95 bg-red-500 py-1 text-xs rounded font-bold text-white'>Delete</button>
             </div>
-            <button className="w-full cursor-pointer active:scale-95 bg-red-500 py-1 text-xs rounded font-bold text-white">
-              Delete
-            </button>
-          </div>
+          })}
         </div>
       </div>
     </div>
