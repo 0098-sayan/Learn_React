550b58db1ca1211a19a85589fc7f0f066876b7ce
0098-sayan <maitysayan704@gmail.com>
2025-11-10 20:22:56 +0530

    UI created

--- 16_context_api/src/App.jsx
+++ 16_context_api/src/App.jsx
@@ -1,9 +1,13 @@
-
-
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
 
-export default App
+        <div className="w-full max-w-sm mx-auto"></div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
