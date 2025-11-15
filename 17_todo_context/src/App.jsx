0221e295ba58d5d5ab62bfdbf30555c69fa13181
0098-sayan <maitysayan704@gmail.com>
2025-11-15 22:08:49 +0530

    Addtodo function added

--- 17_todo_context/src/App.jsx
+++ 17_todo_context/src/App.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
+import { TodoProvider } from "./contexts";
+
 const App = () => {
+  const [todos, setTodos] = useState([]);
+  const addTodo = (todo) => {
+    setTodos((prev) => [{ id: Date.now(), ...todo }, ...prev]);
+  };
+
   return (
-    <>
+    <TodoProvider
+      value={{ todos, addTodo, updateTodo, deleteTodo, togglecomplete }}
+    >
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
@@ -12,7 +22,7 @@ const App = () => {
           </div>
         </div>
       </div>
-    </>
+    </TodoProvider>
   );
 };
 
