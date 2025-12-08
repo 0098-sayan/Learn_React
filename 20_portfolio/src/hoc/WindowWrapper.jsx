2fb42c5e32244463aaf1e7157e7c9454804568bb
0098-sayan <maitysayan704@gmail.com>
2025-12-08 20:13:09 +0530

    added window animation

--- 20_portfolio/src/hoc/WindowWrapper.jsx
+++ 20_portfolio/src/hoc/WindowWrapper.jsx
@@ -1,6 +1,8 @@
 import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import useWindowStore from "@store/window.js";
 import { useLayoutEffect, useRef } from "react";
+import { Scale } from "lucide-react";
 
 const WindowWrapper = (Component, windowKey) => {
   const Wrapped = (props) => {
@@ -13,6 +15,12 @@ const WindowWrapper = (Component, windowKey) => {
       if (!el || !open) return;
 
       el.style.display = "block";
+
+      gsap.fromTo(
+        el,
+        { Scale: 0.8, opacity: 0, y: 40 },
+        { Scale: 1, opacity: 1, duration: 0.4, ease: "power3.out" }
+      );
     }, [isOpen]);
 
     useLayoutEffect(() => {
