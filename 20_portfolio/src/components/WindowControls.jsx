33ac27ec5bb0fe9cb39034afccd113aa27a62b70
0098-sayan <maitysayan704@gmail.com>
2025-12-09 19:01:45 +0530

    window controls done

--- 20_portfolio/src/components/WindowControls.jsx
+++ 20_portfolio/src/components/WindowControls.jsx
@@ -1,5 +1,19 @@
-const WindowControls = () => {
-  return <div>WindowControls</div>;
+import useWindowStore from "@store/window";
+import PropTypes from "prop-types";
+
+const WindowControls = ({ target }) => {
+  const { closeWindow } = useWindowStore();
+  return (
+    <div id="window-controls">
+      <div className="close" onClick={() => closeWindow(target)} />
+      <div className="minimize" />
+      <div className="maximize" />
+    </div>
+  );
+};
+
+WindowControls.propTypes = {
+  target: PropTypes.any.isRequired,
 };
 
 export default WindowControls;
