7fa98a8e3f234320cbdaa2e41462a61d611815e9
0098-sayan <maitysayan704@gmail.com>
2026-01-11 23:22:08 +0530

    Finder option done

--- 20_portfolio/src/windows/Finder.jsx
+++ 20_portfolio/src/windows/Finder.jsx
@@ -2,11 +2,19 @@ import { WindowControls } from "@components";
 import { locations } from "@constants";
 import WindowWrapper from "@hoc/WindowWrapper";
 import useLocationStore from "@store/location";
+import useWindowStore from "@store/window";
 import clsx from "clsx";
 import { Search } from "lucide-react";
 
 const Finder = () => {
+  const {openWindow} = useWindowStore();
   const { activeLocation, setActiveLocation } = useLocationStore();
+
+  const openItem = (item) => {
+    if(item.fileType === "pdf") return openWindow("resume");
+    if(item.kind === "folder") return setActiveLocation(item);
+    if(["fig","url"].includes(item.fileType) && item.href) return window.open(item.href, "_blank");
+  };
 
   const renderList = (items) =>
     items.map((item) => (
@@ -14,9 +22,8 @@ const Finder = () => {
         key={item.id}
         onClick={() => setActiveLocation(item)}
         className={clsx(
-          item.id === activeLocation?.id ? "active" : "not-active",
+          item.id === activeLocation?.id ? "active" : "not-active"
         )}
-        
       >
         <img src={item.icon} alt={item.name} className="w-4" />
         <p className="text-sm font-medium truncate">{item.name}</p>
@@ -37,11 +44,21 @@ const Finder = () => {
           </div>
           <div>
             <h3>Work</h3>
-            <ul>
-              {renderList(locations.work.children)}
-            </ul>
+            <ul>{renderList(locations.work.children)}</ul>
           </div>
         </div>
+        <ul className="content">
+          {activeLocation?.children.map((item) => (
+            <li
+              key={item.id}
+              className={item.position}
+              onClick={() => openItem(item)}
+            >
+              <img src={item.icon} alt={item.name} />
+              <p>{item.name}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
